550f456324144d29a2b097db492cfb107dac24e6
AbhinavGor <abhinav20016@gmail.com>
2021-11-16 00:25:59 +0530

    ADD: init files

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require("cors");
+const cookieParser = require("cookie-parser")
+const path = require("path");
+const session = require("express-session");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}).then(()=> {
+    console.log("MONGO DB CONNECTED!")
+}).catch((e)=>console.log("Cannot Connect to Mongo",e));
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 60000 }
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}.`);
+})
