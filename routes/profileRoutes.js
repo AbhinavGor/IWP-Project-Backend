482b0c2fa12436af1654443dcd1294248b2aac27
AbhinavGor <abhinav20016@gmail.com>
2021-11-26 13:24:36 +0530

    ADD: profile auth route

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,18 +1,9 @@
 const router = require('express').Router();
 const {auth} = require('../middleware/auth');
+
 router.get('/', auth, async (req, res) => {
     try {
-        const token = req.header("Authorization").replace("Bearer ", "");
-
-        const decoded = jwt.verify(token, process.env.SESSION_SECRET);
-        
-        const user = await User.findOne({ email: decoded.email });
-
-        if(user){
-            res.status(200).send(user);
-        }else{
-            res.status(404).send({'message': 'No user found!'});
-        }
+        res.send(req.user);
     } catch (error) {
         res.status(500).send(error);
     }
